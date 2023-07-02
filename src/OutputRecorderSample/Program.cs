71406af16a822e06f546efa51d3fef203fb8b113
CountrySideEngineer <deep-sleeper@nifty.com>
2023-07-02 21:58:54 +0900

    Update sample program using OutputRecorder library.

--- src/OutputRecorderSample/Program.cs
+++ src/OutputRecorderSample/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using OutputRecorder;
 
 namespace OutputRecorderSample
@@ -12,7 +13,11 @@ namespace OutputRecorderSample
 		static void Main(string[] args)
 		{
 			string appPath = @"E:\development\OutputRecorder\SampleConsole\SampleConsole\bin\Debug\SampleConsole.exe";
+			string fileNameWithouExt = System.IO.Path.GetFileNameWithoutExtension(appPath);
+			string dirPath = System.IO.Directory.GetParent(appPath).FullName;
+			string outputPath = $@"{dirPath}\{fileNameWithouExt}.log";
 			Recorder recorder = new Recorder();
+			recorder.OutputFilePath = outputPath;
 			recorder.Record(appPath);
 		}
 	}
