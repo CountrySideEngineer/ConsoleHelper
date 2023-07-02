0762e1aee7d00b8af7c95d51650f686ea33dd6c4
CountrySideEngineer <deep-sleeper@nifty.com>
2023-07-02 22:17:22 +0900

    Add StandardOutputData class. -Add the class, output received data into the console.

---
+++ src/OutputRecorder/Output/StandardOutputData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutputRecorder.Output
+{
+	internal class StandardOutputData
+	{
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public StandardOutputData() { }
+
+		public void DataReceivedEventHandler(object sender, DataReceivedEventArgs e)
+		{
+			Console.WriteLine(e.Data);
+		}
+
+		public void ErrorReceivedEventHandler(object sender, DataReceivedEventArgs e)
+		{
+			Console.Error.WriteLine(e.Data);
+		}
+
+		public void DataReceivedFinishedEventHandler(object sender, EventArgs e) { }
+	}
+}
