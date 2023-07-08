3304410ff1ba0ccb9398022c2c6fd9f6dbe9c416
CountrySideEngineer <deep-sleeper@nifty.com>
2023-07-09 07:23:11 +0900

    Update ProcessRunner class. -Modify bugs found in test. The bug is that the application requests user to press enter key two times to return control to sub process if the process uses "ReadLine" method to get user input. To fix it, update the class to send line feed code '\r' if the key code user input has been "carriage return", '\r'.

--- src/ConsoleHelperLib/Proc/ProcessRunner.cs
+++ src/ConsoleHelperLib/Proc/ProcessRunner.cs
@@ -113,15 +113,18 @@ namespace ConsoleHelperLib.Proc
 					string inputText = string.Empty;
 					do
 					{
-						ConsoleKeyInfo keyInfo = Console.ReadKey();
-						stdin.Write(keyInfo.KeyChar);
+						var inputData = Console.ReadKey();
+						stdin.Write(inputData.KeyChar);
+						if (inputData.KeyChar.Equals('\r'))
+						{
+							stdin.Write('\n');
+						}
 					} while ((inputText != null) && (true == _isContinue));
 				}
 			}
 			catch (InvalidOperationException)
 			{
 				Console.WriteLine("Sub process request invalid operation");
-
 			}
 			proc.WaitForExit();
 		}
