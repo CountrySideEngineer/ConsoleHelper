00ee15ae33d4ab912ace95073e642572cb134f5c
CountrySideEngineer <deep-sleeper@nifty.com>
2023-07-08 17:47:54 +0900

    Update AOutputProcRunner class. -Add default constructor. -Add comment to methods of the class.

--- src/OutputRecorderDll/Proc/AOutputProcRunner.cs
+++ src/OutputRecorderDll/Proc/AOutputProcRunner.cs
@@ -16,18 +16,43 @@ namespace OutputRecorder.Proc
 		public delegate void DataReceiveFinishedEventHandler(object sender, EventArgs e);
 		public event EventHandler ReceiveFinished;
 
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public AOutputProcRunner() { }
+
+		/// <summary>
+		/// Abstract method to run process.
+		/// </summary>
+		/// <param name="procName"></param>
+		/// <param name="procArgs"></param>
 		public abstract void Run(string procName, string procArgs);
 
+		/// <summary>
+		/// Event handler to handle receiving standard output.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		protected virtual void StandardDataReceived(object sender, DataReceivedEventArgs e)
 		{
 			ReceiveStandardData?.Invoke(this, e);
 		}
 
+		/// <summary>
+		/// Event handler to handle receiving error output.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		protected virtual void ErrorDataReceived(object sender, DataReceivedEventArgs e)
 		{
 			ReceiveErrorData?.Invoke(this, e);
 		}
 
+		/// <summary>
+		/// Event handler to receive the process finished.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		protected virtual void DataReceiveFinished(object sender, EventArgs e)
 		{
 			ReceiveFinished?.Invoke(this, e);
