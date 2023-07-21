d037064c103466e442a847797f3c1a2b675b442f
CountrySideEngineer <deep-sleeper@nifty.com>
2023-07-21 09:44:58 +0900

    Add sample program in VC++ language.

---
+++ Sample/ConsoleHelperSampleVC/ConsoleHelperSampleVC.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+#include <Windows.h>
+#include <tchar.h>
+using namespace std;
+
+int main()
+{
+    _tprintf(_T("Input a line under 1024 characters.\n"));
+    flush(std::cout);
+
+    TCHAR   readline[1024] = { 0 };
+    _getts_s(readline, 1024);
+
+    _tprintf(_T("User inputs \"%s\"\n"), readline);
+    flush(std::cout);
+
+    TCHAR   getch = '\0';
+    do {
+        getch = _gettchar();
+        _tprintf(_T("Input = %c\n"), getch);
+        flush(std::cout);
+    } while (0 < getch);
+
+
+    return 0;
+}
