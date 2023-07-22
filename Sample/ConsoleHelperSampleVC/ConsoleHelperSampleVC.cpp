e4793033e691a46f74767d1bf689f80afaf615b4
CountrySideEngineer <deep-sleeper@nifty.com>
2023-07-22 16:41:54 +0900

    Update sample program.

--- Sample/ConsoleHelperSampleVC/ConsoleHelperSampleVC.cpp
+++ Sample/ConsoleHelperSampleVC/ConsoleHelperSampleVC.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 int main()
 {
+    _tprintf(_T("Read standard input by gets(_getts_s) method.\n"));
     _tprintf(_T("Input a line under 1024 characters.\n"));
     flush(std::cout);
 
@@ -18,13 +19,14 @@ int main()
     _tprintf(_T("User inputs \"%s\"\n"), readline);
     flush(std::cout);
 
+    _tprintf(_T("Read standard input by getchar(_gettchar) method.\n"));
+    flush(std::cout);
     TCHAR   getch = '\0';
     do {
         getch = _gettchar();
         _tprintf(_T("Input = %c\n"), getch);
         flush(std::cout);
     } while (0 < getch);
-
 
     return 0;
 }
